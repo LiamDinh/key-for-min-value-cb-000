22029a607804c33528b6fc57bf60e815590c2a15
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-27 03:40:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
    if smallest_value == nil || value < smallest_value
      smallest_value = value
-     smallest_key = key
+     smallest_key = name 
   end
 end
 smallest_key
